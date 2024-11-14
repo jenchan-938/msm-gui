5c03904bccd48edd73d1f44965e6edf92d332bc7
jenchan-938 <jchan5@uchicago.edu>
2024-11-14 06:15:00 +0000

    delete_directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,24 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def create
+    d=Director.new
+    d.name=params.fetch("name")
+    d.bio=params.fetch("bio")
+    d.dob=params.fetch("dob")
+    d.image=params.fetch("image")
+    d.save
+    redirect_to("/directors") #error says key already duplicated
+
+  end
+
+  def delete
+    the_id=params.fetch("an_id")
+    matching_record=Director.where({:id=>the_id})
+    director=matching_record.at(0)
+    director.destroy
+    redirect_to("/directors")
+
+  end
 end
